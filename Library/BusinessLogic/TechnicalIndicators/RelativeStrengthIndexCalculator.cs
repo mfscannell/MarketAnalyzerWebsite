4ec89585df5820d4520e87a0466372ec0403dbe1
Mark <mfscannell@yahoo.com>
2019-05-11 15:03:44 -0500

    finished RSI calculation

---
+++ Library/BusinessLogic/TechnicalIndicators/RelativeStrengthIndexCalculator.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceWebsite.Library.BusinessLogic.TechnicalIndicators
+{
+    public class RelativeStrengthIndexCalculator
+    {
+        #region Private Fields
+
+        //private double[] values;
+
+        //private double[] changes;
+
+        //private double[] gains;
+
+        //private double[] losses;
+
+        //private double[] averageGains;
+
+        //private double[] averageLosses;
+
+        private Queue<double> values;
+
+        private Queue<double> changes;
+
+        private Queue<double> gains;
+
+        private Queue<double> losses;
+
+        private Queue<double> averageGains;
+
+        private Queue<double> averageLosses;
+
+        /// <summary>
+        /// The number of values inserted so far.
+        /// </summary>
+        private int numberOfValuesInserted;
+
+        private readonly int numberOfRecordsForCalculation;
+
+        #endregion
+
+        #region Constructors
+
+        public RelativeStrengthIndexCalculator(int numberOfRecordsForCalculation)
+        {
+            this.numberOfRecordsForCalculation = numberOfRecordsForCalculation;
+
+            this.values = new Queue<double>();
+            this.changes = new Queue<double>();
+            this.gains = new Queue<double>();
+            this.losses = new Queue<double>();
+            this.averageGains = new Queue<double>();
+            this.averageLosses = new Queue<double>();
+
+            this.numberOfValuesInserted = 0;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public double CalculateRelativeStrengthIndex(double latestValue)
+        {
+            if (this.values.Count == this.numberOfRecordsForCalculation)
+            {
+                this.values.Dequeue();
+                this.changes.Dequeue();
+                this.gains.Dequeue();
+                this.losses.Dequeue();
+                this.averageGains.Dequeue();
+                this.averageLosses.Dequeue();
+            }
+
+            var change = this.numberOfValuesInserted == 0 ? 0 : latestValue - this.values.Last();
+            var gain = Math.Max(0, change);
+            var loss = Math.Abs(Math.Min(change, 0));
+            var avgGain = 0.0;
+            var avgLoss = 0.0;
+
+            if (this.numberOfValuesInserted == this.numberOfRecordsForCalculation)
+            {
+                avgGain = (this.gains.Sum() + gain) / this.numberOfRecordsForCalculation;
+                avgLoss = (this.losses.Sum() + loss) / this.numberOfRecordsForCalculation;
+            } else if (this.numberOfValuesInserted == this.numberOfRecordsForCalculation + 1)
+            {
+                //about to insert the 16th+ record
+                avgGain = ((this.numberOfRecordsForCalculation - 1) * this.averageGains.Last() + 1 * gain) / 
+                    this.numberOfRecordsForCalculation;
+                avgLoss = ((this.numberOfRecordsForCalculation - 1) * this.averageLosses.Last() + 1 * loss) /
+                    this.numberOfRecordsForCalculation;
+            }
+
+            this.values.Enqueue(latestValue);
+            this.changes.Enqueue(change);
+            this.gains.Enqueue(gain);
+            this.losses.Enqueue(loss);
+            this.averageGains.Enqueue(avgGain);
+            this.averageLosses.Enqueue(avgLoss);
+
+
+
+            // UPDATE NUMBER OF RECORDS INSERTED
+            if (this.numberOfValuesInserted < this.numberOfRecordsForCalculation + 1)
+            {
+                this.numberOfValuesInserted++;
+            }
+
+
+
+
+            if (this.numberOfValuesInserted <= this.numberOfRecordsForCalculation)
+            {
+                return 0;
+            }
+            else if (Double.Equals(avgLoss, 0))
+            {
+                return 100;
+            }
+            else
+            {
+                var relativeStrength = avgGain / avgLoss;
+
+                return 100 - 100 / (1 + relativeStrength);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ UnitTests/Library/BusinessLogicTests/TechnicalIndicators/RelativeStrengthIndexCalculatorTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FinanceWebsite.Library.BusinessLogic.TechnicalIndicators;
+
+namespace UnitTests.FinanceWebsite.Library.BusinessLogicTests.TechnicalIndicators
+{
+    [TestClass]
+    public class RelativeStrengthIndexCalculatorTests
+    {
+        [TestMethod]
+        public void TestCalculateRelativeStrengthIndex()
+        {
+            var rsiCalculator = new RelativeStrengthIndexCalculator(14);
+
+            var rsi = rsiCalculator.CalculateRelativeStrengthIndex(44.34);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(44.09);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(44.15);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(43.61);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(44.33);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(44.83);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(45.10);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(45.42);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(45.84);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(46.08);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(45.89);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(46.03);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(45.61);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(46.28);
+            Assert.AreEqual(0, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(46.28);
+            Assert.AreEqual(70.46, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(46);
+            Assert.AreEqual(66.25, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(46.03);
+            Assert.AreEqual(66.48, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(46.41);
+            Assert.AreEqual(69.35, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(46.22);
+            Assert.AreEqual(66.29, rsi, 0.005);
+
+            rsi = rsiCalculator.CalculateRelativeStrengthIndex(45.64);
+            Assert.AreEqual(57.92, rsi, 0.005);
+        }
+    }
+}
