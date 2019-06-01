02065ee477fb5263e0fe4b42fba7a7160685c913
Mark <mfscannell@yahoo.com>
2019-06-01 09:15:16 -0500

    added webapi for data to MVC project

---
+++ FinanceWebsite.MVC/App_Start/FormatterConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+using Newtonsoft.Json.Serialization;
+
+using WebActivatorEx;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(FinanceWebsite.MVC.App_Start.FormatterConfig), "ConfigFormat")]
+namespace FinanceWebsite.MVC.App_Start
+{
+    public static class FormatterConfig
+    {
+        public static void ConfigFormat()
+        {
+            var config = GlobalConfiguration.Configuration;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
